a9bf1d2fad7e5acac708775c1e6c14b49ea4f071
SaiRekha555 <128289460+SaiRekha555@users.noreply.github.com>
2025-03-09 15:11:20 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
-import { Navbar } from "./components/navbar";
-import { Auth } from "./pages/auth";
-import { CreateRecipe } from "./pages/create-recipe";
-import { Home } from "./pages/home";
-import { SavedRecipes } from "./pages/saved-recipes";
+import { useState, useEffect } from "react";
+
+import Register from "./Pages/Register";
+import Home from "./Pages/home";
+import { CreateRecipe } from "./Pages/create-recipe";
+import { SavedRecipes } from "./Pages/saved-recipes";
+import Navbar from "./Components/navbar";
+import Login from ". /Pages/login";
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if user is logged in
+    const user = localStorage.getItem("username");
+    setIsLoggedIn(!!user);
+  }, []);
+
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create-recipe" element={<CreateRecipe />} />
-          <Route path="/saved-recipes" element={<SavedRecipes />} />
+    <Router>
+      <Navbar isLoggedIn={isLoggedIn}/>
+      <Routes>
+        {/* Redirect to Login if not logged in */}
+        <Route path="/" element={<Home /> } />
+        <Route path="/create-recipe" element={<CreateRecipe/>} />
+          <Route path="/saved-recipes" element={<SavedRecipes/>} />
           <Route path="/auth" element={<Auth />} />
-        </Routes>
-      </Router>
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/register" element={<Register setIsLoggedIn={setIsLoggedIn} />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
