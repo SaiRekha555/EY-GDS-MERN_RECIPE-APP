3f5c1edb0b4afdf847a04acc3980f331e6af49ed
SaiRekha555 <128289460+SaiRekha555@users.noreply.github.com>
2025-03-09 11:41:26 +0530

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://SaiRekhaKollapudi:Rekha123@cluster0.k2bnb.mongodb.net/rekha555?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(
+    console.log("DB connected...")
+  )
+  app.listen(3001, () => console.log("Server started"));
