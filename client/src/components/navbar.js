eedfb9f99342c849cc23cd06212c13a24c93547d
SaiRekha555 <128289460+SaiRekha555@users.noreply.github.com>
2025-03-09 15:47:20 +0530

    Update navbar.js

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
 export const Navbar = () => {
   const [cookies, setCookies] = useCookies(["access_token"]);
   const navigate = useNavigate();
